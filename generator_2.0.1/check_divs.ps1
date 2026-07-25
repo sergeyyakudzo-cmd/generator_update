6873ee889365c796a912664386bea6af1b8bc898
sergeyyakudzo-cmd <sergey.yakudzo@gmail.com>
2026-07-25 11:08:43 +0300

    Delete generator_2.0.1 directory

--- generator_2.0.1/check_divs.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content 'C:\Проекты\Генератор\generator_master\generator_new.html' -Raw -Encoding UTF8
-$opens = ([regex]::Matches($content, '<div[\s>]')).Count
-$closes = ([regex]::Matches($content, '</div>')).Count
-Write-Output "Opens: $opens, Closes: $closes, Diff: $($opens - $closes)"
